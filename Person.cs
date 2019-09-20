fd83766479e462ddb56e9ec96433b14775ed5bad
Szanyi Krisztián <szkript@gmail.com>
2019-09-20 16:04:42 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass {
+    class Person {
+        string Name;
+        int BirthDate;
+        Gender Gender;
+
+        public Person(string Name, int BirthDate, Gender Gender) {
+            this.Name = Name;
+            this.BirthDate = BirthDate;
+            this.Gender = Gender;
+
+        }
+
+        public override string ToString() {
+            return Name.ToString() + " , birth year: " + BirthDate.ToString() + ", gender: " + Gender.ToString();
+        }
+    }
+    enum Gender {
+        Male,
+        Female
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass {
+    class CreateClass {
+        static void Main(string[] args) {
+            Person person = new Person("János", 1992, Gender.Male);
+            Console.WriteLine(person);
+
+            Console.ReadLine();
+        }
+    }
+}
+
